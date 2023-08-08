4d9e01c06f5c7cff2f5888b025a8dfd1d7c5f25d
lpeixotomagalhaes <134023267+lpeixotomagalhaes@users.noreply.github.com>
2023-08-08 17:13:08 -0300

    D1079

--- Estrutura-Sequencial/aula1/aula1.java
+++ Estrutura-Sequencial/aula1/aula1.java
@@ -1,12 +1,28 @@
 package aula1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class aula1 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a);
+        int n = sc.nextInt();
+
+        double[] vetor = new double[3];
+        double[] resultado = new double[n];
+
+        for(int i = 0; i < n; i++) {
+            vetor[0] = sc.nextDouble();
+            vetor[1]= sc.nextDouble();
+            vetor[2] = sc.nextDouble();
+            resultado[i] = ((vetor[0]*2)+(vetor[1]*3)+(vetor[2]*5))/10;
+        }
+
+        for (int i = 0;i < n;i++){
+            System.out.printf("%.1f%n", resultado[i]);
+        }
+
     }
 }
 
