3c5c2641013b4bff1a3028d27f914686edb84adf
lpeixotomagalhaes <134023267+lpeixotomagalhaes@users.noreply.github.com>
2023-08-08 17:34:29 -0300

    D1095

--- Beecrowd/D1095/Main.java
+++ Beecrowd/D1095/Main.java
@@ -2,6 +2,13 @@ package D1095;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("lucas");
+        int i = 1;
+        int j = 60;
+
+        while (j >= 0) {
+            System.out.printf("I=%d J=%d%n", i, j);
+            i += 3;
+            j -= 5;
+        }
     }
 }
